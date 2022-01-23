0fbf73baa7acaed74c1677f91ec4f57ef62ba794
Samuel-Xavier <samueloliveira.xavier@hotmail.com>
2022-01-23 15:37:56 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql")
+
+//local
+//const db = mysql.createConnection({
+//    host: "localhost",
+//    //port: "",
+//    user: "root",
+//    password: "123456",
+//    database: "bagaco2",
+//});
+
+//RDS AWS
+const db = mysql.createConnection({
+    host: "mysqlaws.co1ozssslcqk.us-east-2.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "setc2430",
+    database: "my_db",
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(err.message);
+        return;
+    }
+    console.log("Database conected.");
+});
